710d8cb1911d77ffc1c3f8fdc6dee7d0bdd14460
Mike Oscar <60878641+mikehappythoughts@users.noreply.github.com>
2021-12-15 00:42:30 +0000

    Delete .nuxt directory

--- .nuxt/composition-api/globals.mjs
+++
@@ -1,5 +0,0 @@
-export const isFullStatic = false
-export const staticPath = "/Users/unstopable4/Desktop/NUXT projects/Nuxt-covid19-dashboard/.nuxt/static-json"
-export const publicPath = "/covid/"
-export const globalContext = "__NUXT__"
-export const globalNuxt = "$nuxt"
